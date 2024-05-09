988cfbb9e3eab1d62e6921014becdaee11062150
Nikita Tyunyayev <nikita.tyunyayev@uclouvain.be>
2024-05-09 10:25:02 +0000

    fixed formating

--- src/plugins/kvs/kvs_test.c
+++ src/plugins/kvs/kvs_test.c
@@ -14,16 +14,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <stdbool.h>
 #include <vat/vat.h>
 #include <vlibapi/api.h>
 #include <vlibmemory/api.h>
 #include <vppinfra/error.h>
-#include <stdbool.h>
 
 #define __plugin_msg_base kvs_test_main.msg_id_base
 #include <vlibapi/vat_helper_macros.h>
 
-uword unformat_sw_if_index (unformat_input_t * input, va_list * args);
+uword unformat_sw_if_index (unformat_input_t *input, va_list *args);
 
 /* Declare message IDs */
 #include <kvs/kvs.api_enum.h>
@@ -41,7 +41,7 @@ kvs_test_main_t kvs_test_main;
 static int
 api_kvs_enable_disable (vat_main_t *vam)
 {
-  unformat_input_t * i = vam->input;
+  unformat_input_t *i = vam->input;
   int enable_disable = 1;
   u32 sw_if_index = ~0;
   vl_api_kvs_enable_disable_t *mp;
@@ -51,13 +51,13 @@ api_kvs_enable_disable (vat_main_t *vam)
   while (unformat_check_input (i) != UNFORMAT_END_OF_INPUT)
     {
       if (unformat (i, "%U", unformat_sw_if_index, vam, &sw_if_index))
-          ;
-        else if (unformat (i, "sw_if_index %d", &sw_if_index))
-          ;
+	;
+      else if (unformat (i, "sw_if_index %d", &sw_if_index))
+	;
       else if (unformat (i, "disable"))
-          enable_disable = 0;
+	enable_disable = 0;
       else
-          break;
+	break;
     }
 
   if (sw_if_index == ~0)
@@ -67,12 +67,12 @@ api_kvs_enable_disable (vat_main_t *vam)
     }
 
   /* Construct the API message */
-  M(KVS_ENABLE_DISABLE, mp);
+  M (KVS_ENABLE_DISABLE, mp);
   mp->sw_if_index = ntohl (sw_if_index);
   mp->enable_disable = enable_disable;
 
   /* send it... */
-  S(mp);
+  S (mp);
 
   /* Wait for a reply... */
   W (ret);
